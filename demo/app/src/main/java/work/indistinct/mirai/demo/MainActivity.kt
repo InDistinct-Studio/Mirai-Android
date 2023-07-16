c23127e5cff4e63a75f4a4930cf7dce57ff44828
Verachad <verachad@fastwork.co>
2023-07-16 22:18:26 +0700

    wip

--- demo/app/src/main/java/work/indistinct/mirai/demo/MainActivity.kt
+++ demo/app/src/main/java/work/indistinct/mirai/demo/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : ComponentActivity() {
                     viewState = viewModel.viewState.value
                 )
             }
-//            ekycWithIdCardGraph(navController)
         }
     }
 
--- demo/app/src/main/java/work/indistinct/mirai/demo/common/Components.kt
+++ demo/app/src/main/java/work/indistinct/mirai/demo/common/Components.kt
@@ -1,5 +1,11 @@
 package work.indistinct.mirai.demo.common
 
+import android.Manifest
+import android.content.Context
+import androidx.camera.core.CameraSelector
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,9 +14,20 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,6 +46,76 @@ fun AppBar(
         title = { Text(title) },
         navigationIcon = it
     )
+    }
+}
+
+
+suspend fun Context.createVideoCaptureUseCase(
+    lifecycleOwner: LifecycleOwner,
+    cameraSelector: CameraSelector,
+    previewView: PreviewView
+): VideoCapture<Recorder> {
+    val preview = Preview.Builder()
+        .build()
+        .apply { setSurfaceProvider(previewView.surfaceProvider) }
+
+    val qualitySelector = QualitySelector.from(
+        Quality.FHD,
+        FallbackStrategy.lowerQualityOrHigherThan(Quality.FHD)
+    )
+    val recorder = Recorder.Builder()
+        .setExecutor(mainExecutor)
+        .setQualitySelector(qualitySelector)
+        .build()
+    val videoCapture = VideoCapture.withOutput(recorder)
+
+    val cameraProvider = getCameraProvider()
+    cameraProvider.unbindAll()
+    cameraProvider.bindToLifecycle(
+        lifecycleOwner,
+        cameraSelector,
+        preview,
+    )
+
+    return videoCapture
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraView() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    val permissionState = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.CAMERA,
+        )
+    )
+
+    val previewView: PreviewView = remember { PreviewView(context) }
+
+    val cameraSelector: MutableState<CameraSelector> = remember {
+        mutableStateOf(CameraSelector.DEFAULT_BACK_CAMERA)
+    }
+
+    LaunchedEffect(Unit) {
+        permissionState.launchMultiplePermissionRequest()
+    }
+
+    PermissionsRequired(
+        multiplePermissionsState = permissionState,
+        permissionsNotGrantedContent = { /* ... */ },
+        permissionsNotAvailableContent = { /* ... */ }
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            AndroidView(
+                factory = { previewView },
+                modifier = Modifier.fillMaxSize()
+            )
+
+        }
     }
 }
 
